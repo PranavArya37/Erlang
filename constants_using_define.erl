08e111ba9da33ae5d5b86ebb25086315f4211fc8
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2022-06-13 02:57:13 +0530

    Update constants_using_define.erl

--- constants_using_define.erl
+++ constants_using_define.erl
@@ -1,1 +1,15 @@
+-module(constants_using_define).
+-export([showConstants/0]).
+
+-define(N, 37).
+-define(M, "Pranav").
+-define(SQUARED(X), X*X).
+-define(ADD(A,B), A+B).
+
+showConstants() ->
+	io:format("N = ~p ~n", [?N]),
+	io:format("M = ~p ~n", [?M]),
+	io:format("~p ~n", [?SQUARED(7)]),
+	io:format("~p ~n", [?ADD(2,3)]).
+
 
