7280f75b4b0ade13f2ddbce7bb47ada16324ee35
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2022-06-28 11:04:19 +0530

    Add files via upload

---
+++ Week 2/Codes Week 2/linkandmonitor.erl
@@ -0,0 +1,20 @@
+-module(linkandmonitor).
+-export([myproc/0,chain/1]).
+
+myproc() ->
+	timer:sleep(5000),
+	exit(reason).
+	
+chain(0) ->
+	receive
+		_ -> ok
+	after 2000 ->
+		exit("chain dies here")
+	end;
+chain(N) ->
+	Pid = spawn(fun() -> chain(N-1) end),
+	link(Pid),
+	receive
+		_ -> ok
+	end.
+
